81b43f96b87fcb5b4756abdaea687bc0945d5bbb
Sabrina Coelho <sabrinateodoro1019@hotmail.com>
2021-09-20 22:06:40 -0300

    Prova

--- src/br/edu/univas/main/Questao1.java
+++ src/br/edu/univas/main/Questao1.java
@@ -1,5 +1,37 @@
 package br.edu.univas.main;
 
-public class Questao1 {
+import java.util.Scanner;
 
+public class Questao1 {
+	public static double menorDetec(double n[]) {
+		double menor = n[0];
+		for(int i = 1;i <= 24;i++) {
+			if(n[i] < menor) {
+				menor = n[i];
+			}
+		}
+		return menor;
+	}
+	public static double maiorDetec(double n[]) {
+		double maior = n[0];
+		for(int i = 1;i <= 24;i++) {
+			if(n[i] > maior) {
+				maior = n[i];
+			}
+		}
+		return maior;
+	}
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double[] nota = new double[25];
+		for(int i = 0;i <= 24;i++) {
+			System.out.println("Digite a nota do "+(i + 1)+"° aluno:");
+			nota[i] = in.nextDouble();
+		}
+		double menor = menorDetec(nota);
+		double maior = maiorDetec(nota);
+		System.out.println("Menor nota da prova: "+ menor);
+		System.out.println("Maior nota da prova: "+ maior);
+		in.close();
+	}
 }
--- src/br/edu/univas/main/Questao2.java
+++ src/br/edu/univas/main/Questao2.java
@@ -1,5 +1,39 @@
 package br.edu.univas.main;
 
-public class Questao2 {
+import java.util.Scanner;
 
+public class Questao2 {
+	public static boolean nomeRepetido(String n, String nomes[]) {
+		for(int i = 0; i <= nomes.length; i++) {
+			for(int j = 0; j <= n.length(); j++) {
+				char h1 = n.charAt(j);
+				char h2 = nomes[i].charAt(j);
+				if(h1 == h2) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String[] convidado = new String[4];
+		boolean permition;
+		String verif;
+		for(int i = 0; i <= 2;i++) {
+			if(i == 0) {
+				convidado[0] = in.nextLine();
+				continue;
+			}
+			verif = in.nextLine().toUpperCase();
+			permition = nomeRepetido(verif, convidado);
+			if(permition) {
+				convidado[i] = verif;
+			}
+			else {
+				System.out.println("nome reprtido detec");
+			}
+		}
+		in.close();
+	}
 }
